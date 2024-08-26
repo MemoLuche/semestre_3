16d3c6afe3900225d30b8aefa52cfd450af5dfea
MemoLuche <guillermogp08@gmail.com>
2024-08-26 14:16:57 -0600

    Adgregar archivos

---
+++ src/python/clase_listas.py
@@ -0,0 +1,11 @@
+marcas_autos = ['BMW', 'Audi', 'Volkswagen', 'Chevrolet']
+print(marcas_autos)
+print(marcas_autos[0])
+print(marcas_autos[-1])
+print(f'La tercera marca de la lista es {marcas_autos[2]}')
+
+print('Lista original')
+print(marcas_autos)
+marcas_autos_organizada = sorted(marcas_autos)
+print('Lista organizada')
+print(marcas_autos_organizada)
---
+++ src/python/foreach.py
@@ -0,0 +1,17 @@
+magos = {'Alicia', 'David', 'Carolina'}
+
+for mago in magos:
+    print(mago)
+
+print('\n')
+print(magos)
+
+pares = list(range(2,11,2))
+print(pares)
+
+cuadrados = list()
+
+for num in range (1,11):
+    cuadrados.append(num**2)
+
+print(cuadrados)

9caff03fa820d107a7b21ad077936b287990dd9f
MemoLuche <guillermogp08@gmail.com>
2024-08-26 14:18:04 -0600

    update

--- src/python/foreach.py
+++ src/python/foreach.py
@@ -10,8 +10,9 @@ pares = list(range(2,11,2))
 print(pares)
 
 cuadrados = list()
-
 for num in range (1,11):
     cuadrados.append(num**2)
-
 print(cuadrados)
+
+cuadrados2 = [valor ** 2 for valor in range in (1,11)]
+print(cuadrados2)
